4af307d5a0e9fd03c164c1ab043e879ba4a266a2
midCenturyDude7 <mgriffes@gmail.com>
2021-06-13 18:33:43 -0700

    minor edit to include REST methods

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
