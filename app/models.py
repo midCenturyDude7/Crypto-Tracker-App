ed51485831a4e9b35a61f627b6f5471fd603df3b
midCenturyDude7 <mgriffes@gmail.com>
2021-06-15 15:43:31 -0700

    rolling changes back

--- app/models.py
+++ app/models.py
@@ -9,10 +9,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    cryptos = db.relationship('Profile', backref='crypto', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -26,11 +26,23 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class Post(db.Model):
+class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'<Profile {self.username}>'
+
+# Miguel's table: keeping it here for now
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     body = db.Column(db.String(140))
+#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#
+#     def __repr__(self):
+#         return '<Post {}>'.format(self.body)
