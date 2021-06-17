e603a08f948201d7eae2f5a292fc6d4a22e8cf91
midCenturyDude7 <mgriffes@gmail.com>
2021-06-16 22:12:37 -0700

    updating to include user profile page and back end

--- app/models.py
+++ app/models.py
@@ -33,7 +33,6 @@ class Profile(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
     def __repr__(self):
         return f'<Profile {self.username}>'
 
