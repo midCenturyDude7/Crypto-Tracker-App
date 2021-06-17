03555f39cd5a3ed11870886988ac5fe754c7572b
midCenturyDude7 <mgriffes@gmail.com>
2021-06-16 22:26:15 -0700

    mindor edits

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
