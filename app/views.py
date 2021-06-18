7e4363e887d172f00c401e59a356c2c49a0b97a7
midCenturyDude7 <mgriffes@gmail.com>
2021-06-17 20:22:48 -0700

    record time of last visit

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+from datetime import datetime
 
 
 @app.route('/')
@@ -67,3 +68,10 @@ def register():
 def user_profile(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
