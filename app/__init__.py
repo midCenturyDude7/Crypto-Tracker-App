52208bfb2402de2dc4a2fd79cd001b85a18d0fb7
midCenturyDude7 <mgriffes@gmail.com>
2021-06-20 21:10:27 -0700

    adding flask-bootstrap for facelift

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 
@@ -14,6 +16,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
