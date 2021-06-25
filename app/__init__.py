d0e7a2b2b2534cebac4d4cdbd39022a2e4f69c4c
midCenturyDude7 <mgriffes@gmail.com>
2021-06-24 21:52:08 -0700

    adding flask-nav functionality

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 from config import Config
 
 app = Flask(__name__)
@@ -17,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+nav = Nav(app)
 
 
 if not app.debug:
